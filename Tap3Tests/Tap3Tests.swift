5dc6f263f62287918618a09789750086f5e6e403
Michael Adams <madams6216@gmail.com>
2015-09-15 09:05:38 -0400

    Initial Commit

---
+++ Tap3Tests/Tap3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tap3Tests.swift
+//  Tap3Tests
+//
+//  Created by michael adams on 9/15/15.
+//  Copyright (c) 2015 michael adams. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tap3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
